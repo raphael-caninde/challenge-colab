e7a512b13c899a5e1ab229f8ba4f1d09493fad20
raphael-caninde <raphael.caninde@gmail.com>
2023-04-15 17:08:50 -0300

    criação do botão darkmode

---
+++ src/components/DarkMode/index.tsx
@@ -0,0 +1,47 @@
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+
+export function DarkMode() {
+  const { theme, setTheme } = useContext(AppContext);
+
+  function testeTheme() {
+    const newTheme = theme === '' ? 'dark' : '';
+    setTheme(newTheme);
+  }
+
+  return theme === 'dark' ? (
+    <div
+      onClick={testeTheme}
+      className='self-end mt-4 mr-10 hidden sm:flex items-center cursor-pointer
+              bg-gradient-to-r from-yellow-300 to-yellow-600
+              w-14 lg:w-20 h-8 p-1 rounded-full'>
+      <div
+        className={`
+                  flex items-center justify-center
+                  bg-white text-yellow-600
+                  w-6 h-6 rounded-full
+              `}>
+        <FontAwesomeIcon icon={faSun} />
+      </div>
+    </div>
+  ) : (
+    <div
+      onClick={testeTheme}
+      className={`
+            self-end mt-4 mr-10 hidden sm:flex items-center justify-end cursor-pointer
+            bg-gradient-to-r from-gray-500 to-gray-900
+            w-14 lg:w-20 h-8 p-1 rounded-full
+        `}>
+      <div
+        className={`
+                flex items-center justify-center
+                bg-black text-yellow-300
+                w-6 h-6 rounded-full
+            `}>
+        <FontAwesomeIcon icon={faMoon} />
+      </div>
+    </div>
+  );
+}
